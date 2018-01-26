1e9cce8eaf1699d7176e7164b931120ce1d1cfe5
3ndrius <andrzejo1991@gmail.com>
2018-01-26 21:25:57 +0100

    Async read write file

--- readFile.js
+++ readFile.js
@@ -1,7 +1,17 @@
 var fs = require('fs');
 
-var readMe = fs.readFileSync('readMe.txt', 'utf8');
+// var readMe = fs.readFileSync('readMe.txt', 'utf8');
 
-console.log(readMe);
+// console.log(readMe);
 
-var writeMe = fs.writeFileSync('writeMe.txt', readMe);
+// var writeMe = fs.writeFileSync('writeMe.txt', readMe);
+
+fs.readFile('readMe.txt', 'utf8' , function(err, data) {
+    console.log(data);
+
+    fs.writeFile('writeMe.txt', data, function(err) {
+        console.log('Done');
+    });
+});
+
+console.log("What is first ");
