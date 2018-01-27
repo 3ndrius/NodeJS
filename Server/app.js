26f7d7a81dd8bfc66a2f31c06cb2bf1c161cca6e
3ndrius <andrzejo1991@gmail.com>
2018-01-27 19:22:25 +0100

    Created Server

---
+++ Server/app.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+
+    console.log('Request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('This is response test');
+});
+
+server.listen(3000, '127.0.0.1');
+
+console.log('Listening on port 3000 ');
