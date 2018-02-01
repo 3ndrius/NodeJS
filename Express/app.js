f703486777aff25a876ab75e112279bd66bce56d
3ndrius <andrzejo1991@gmail.com>
2018-02-01 22:11:32 +0100

    Listing item ejs

--- Express/app.js
+++ Express/app.js
@@ -13,7 +13,7 @@ app.get('/contact', (req, res)=>{
     res.sendFile(__dirname +'/contact.html');
 });
 
-var data = {age:24, name: 'Josh', job:'programmer'};
+var data = {age:24, name: 'Josh', job:'programmer', hobbies:['eating', 'fishing', 'gaming']};
 
 app.get('/profile/:id', (req, res)=> {
     res.render('profile', { person: req.params.id, data: data });
