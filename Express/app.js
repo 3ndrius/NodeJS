3677ea6bcbe1ffb8dc51ec41ed512886eaeda498
3ndrius <andrzejo1991@gmail.com>
2018-02-01 23:12:25 +0100

    Static

--- Express/app.js
+++ Express/app.js
@@ -4,6 +4,13 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+ app.use('/assets', express.static('assets'));
+
+// app.use('/assets', function(req, res, next) {
+//     console.log(req.url);
+//     next();
+// });
+
 
 app.get('/', (req, res)=>{
     res.render('index');
