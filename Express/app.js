009b85df6eca2473e6d049096787d605a41f1332
3ndrius <andrzejo1991@gmail.com>
2018-02-01 21:37:53 +0100

    Route params

--- Express/app.js
+++ Express/app.js
@@ -6,10 +6,14 @@ var app = express();
 app.get('/', (req, res)=>{
     res.send('This is a home page');
 });
+
 app.get('/contact', (req, res)=>{
     res.send('This is a contact page');
 });
 
+app.get('/profile/:id', (req, res)=> {
+    res.send('You requested to see a profile with a id of: ' + req.params.id);
+});
 
 
 app.listen(3000);
