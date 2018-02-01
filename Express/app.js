86160e11c9106bcb33f31c46ef0b579d7f268e59
3ndrius <andrzejo1991@gmail.com>
2018-02-01 21:33:00 +0100

    Express route

---
+++ Express/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+
+app.get('/', (req, res)=>{
+    res.send('This is a home page');
+});
+app.get('/contact', (req, res)=>{
+    res.send('This is a contact page');
+});
+
+
+
+app.listen(3000);
