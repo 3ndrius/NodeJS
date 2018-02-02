5ea75f63e0744de7f3af7549ce8a3a232190fc74
3ndrius <andrzejo1991@gmail.com>
2018-02-02 19:54:25 +0100

    Query string

--- Express/app.js
+++ Express/app.js
@@ -11,16 +11,19 @@ app.set('view engine', 'ejs');
 //     next();
 // });
 
+var data = {age:24, name: 'Josh', job:'programmer', hobbies:['eating', 'fishing', 'gaming']};
 
 app.get('/', (req, res)=>{
     res.render('index');
 });
 
 app.get('/contact', (req, res)=>{
-    res.render('contact');
+    res.render('contact', {quer:req.query});
+
+    // console.log(req.query);
 });
 
-var data = {age:24, name: 'Josh', job:'programmer', hobbies:['eating', 'fishing', 'gaming']};
+
 
 app.get('/profile/:id', (req, res)=> {
     res.render('profile', { person: req.params.id, data: data });
