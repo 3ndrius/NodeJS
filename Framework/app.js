463ee4a90871ecc4bee334c2fc423e261b4b9f0c
3ndrius <3ndrius@users.noreply.github.com>
2018-08-18 20:56:39 +0200

    Update app.js

--- Framework/app.js
+++ Framework/app.js
@@ -1,14 +1,9 @@
 var fs = require('fs');
 var http = require('http');
-
-
-
 var server = http.createServer(function(req, res) {
-
     console.log('Request was made: ' + req.url);
         if(req.url === '/home' || req.url === '/') 
         {
-           
             res.writeHead(200, {'Content-Type': 'text/html'});
             var MReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
             MReadStream.pipe(res);
@@ -29,10 +24,7 @@ var server = http.createServer(function(req, res) {
             var MReadStream = fs.createReadStream(__dirname + '/404.html', 'utf8');
             MReadStream.pipe(res);
         }
- 
 });
-
 server.listen(3000, '127.0.0.1');
-
 console.log('Listening on port 3000 ');
 
